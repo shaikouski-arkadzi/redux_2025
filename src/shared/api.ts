e85865c92d20d38f84312eff41f674671b7b8382
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-15 21:59:35 +0300

    Add validation server response using zod

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,13 +1,27 @@
+import z from "zod";
+
 const baseUrl = "http://localhost:3002";
+
+const UserDtoSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+});
 
 export const api = {
   getUsers: () => {
-    return fetch(`${baseUrl}/users`).then((response) => response.json());
+    return fetch(`${baseUrl}/users`)
+      .then((response) => response.json())
+      .then((res) => {
+        return UserDtoSchema.array().parse(res);
+      });
   },
   getUser: (userId: string) => {
-    return fetch(`${baseUrl}/users/${userId}`).then((response) =>
-      response.json()
-    );
+    return fetch(`${baseUrl}/users/${userId}`)
+      .then((response) => response.json())
+      .then((res) => {
+        return UserDtoSchema.parse(res);
+      });
   },
   deleteUser: (userId: string) => {
     return fetch(`${baseUrl}/users/${userId}`, {
