d106e3895583e6170549bf392d1a0a74e52fa9d9
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-25 09:05:09 +0300

    Get data from cache in router loader. Anti-pattern. Use only in situation when can't call lots of request. get a lot of caches instead

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
+import { store } from "./store";
 import App from "../modules/app/App";
 import { UsersList } from "../modules/users/UsersList";
 import { SelectedUser } from "../modules/users/SelectedUser";
 import { Counters } from "../modules/counters/Counters";
+import { usersSlice } from "../modules/users/users.slice";
+import { usersApi } from "../modules/users/api/api";
+
+const loadStore = () =>
+  new Promise((resolve) => setTimeout(() => resolve(store), 0));
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +22,12 @@ export const router = createBrowserRouter([
       {
         path: "users",
         element: <UsersList />,
+        loader: () => {
+          loadStore().then(() => {
+            usersApi.endpoints.getUsers.select()(store.getState());
+          });
+          return null;
+        },
       },
       {
         path: "users/:id",
