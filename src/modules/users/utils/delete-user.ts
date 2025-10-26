a6a354cb5925e88cb8b9bf62c2f43644d7abe64e
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-26 08:39:49 +0300

    Get user in from RTK Query in Thunk

--- src/modules/users/utils/delete-user.ts
+++ src/modules/users/utils/delete-user.ts
@@ -5,6 +5,9 @@ import type { UserId } from "../user.types";
 export const deleteUser =
   (userId: UserId): AppThunk<Promise<void>> =>
   async (dispatch, getState, { api, router }) => {
+    const user = await dispatch(
+      usersApi.endpoints.deleteUser.initiate(userId, { track: false })
+    ).unwrap();
     // Run mutation from Thunk
     await dispatch(usersApi.endpoints.deleteUser.initiate(userId));
     await router.navigate("/users");
