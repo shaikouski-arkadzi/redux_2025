9996524526e3b675ed64ba50ecd52cef1ce46a4b
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-30 21:01:54 +0300

    Refactor: Delete counters users function. react query and redux outside the component

--- src/modules/users/delete-counters-users.ts
+++ src/modules/users/delete-counters-users.ts
@@ -1,20 +1,22 @@
 import type { AppThunk } from "../../app/store.types";
+import { queryClient } from "../../shared/api";
 import { resetAction, selectCountersSum } from "../counters";
-import { selectSortedUsers } from "./select-sorted-users";
-import { usersSlice } from "./users.slice";
+import { deleteUser, getUsersQueryOptions, usersBaseKey } from "./api";
+import { sortUsers } from "./select-sorted-users";
+import { sortUsersSlice } from "./sort-users.slice";
 
 export const deleteCountersUsers =
   (): AppThunk<Promise<void>> => async (dispatch, getState) => {
-    const countersSum = selectCountersSum(getState());
-    const sortedUsers = selectSortedUsers(getState());
+    const users = await queryClient.fetchQuery(getUsersQueryOptions());
+    const countersSum = selectCountersSum(getState().counters);
+    const sortType = sortUsersSlice.selectors.sortType(getState());
+
+    const sortedUsers = sortUsers(sortType, users);
 
     const usersToDelete = sortedUsers.slice(0, countersSum);
 
-    dispatch(
-      usersSlice.actions.deleteUsers({
-        userIds: usersToDelete.map((user) => user.id),
-      })
-    );
+    await Promise.all(usersToDelete.map((user) => deleteUser(user.id)));
+    await queryClient.invalidateQueries({ queryKey: usersBaseKey });
 
     dispatch(resetAction());
   };
